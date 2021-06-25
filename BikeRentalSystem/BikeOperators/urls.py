6fb6501063c9263e57a55fce736e5e927c96fba2
Margi <margihpatel308@gmail.com>
2021-06-25 11:48:50 +0530

    made dashbord of operator

--- BikeRentalSystem/BikeOperators/urls.py
+++ BikeRentalSystem/BikeOperators/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.urls.conf import include
 from BikeUsers.views import SignIn
+from .views import *
 
 urlpatterns = [
     path('login/', view=SignIn.as_view(), name='OperatorLogin'),
+    path('dashboard/', view=dash, name='OperatorDashboard'),
+    
 ]
--- BikeRentalSystem/BikeOperators/views.py
+++ BikeRentalSystem/BikeOperators/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+
+def dash(request):
+	return render(request, 'BikeOperators/index.html')
+
