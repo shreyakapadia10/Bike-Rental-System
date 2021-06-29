6e268519b6f242e4adc91ca089a3eba9769f08bd
Roshni710 <roshnivsv007@gmail.com>
2021-06-29 13:57:48 +0530

    available and rented bikes

--- BikeRentalSystem/BikeOperators/urls.py
+++ BikeRentalSystem/BikeOperators/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from django.urls.conf import include
 from BikeUsers.views import SignIn
+from .views import *
 
 urlpatterns = [
     path('login/', view=SignIn.as_view(), name='OperatorLogin'),
+    path('dashboard/', view=dash, name='OperatorDashboard'),
+    path('available_bikes/',bikeinfo),
+    path('rented_bikes/',bikeinfo1),
+    
 ]
--- BikeRentalSystem/BikeOperators/views.py
+++ BikeRentalSystem/BikeOperators/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
-
+from  BikeUsers.models import *
+from .forms import *
 # Create your views here.
 
+
+
+def dash(request):
+	return render(request, 'BikeOperators/index.html')
+
+def bikeinfo(request):
+	#print(request.user.id)
+	bikes= bike.objects.filter(operatorid=request.user)
+
+	return render(request, 'BikeOperators/available_bikes.html',{'viewbike':bikes})
+
+
+def bikeinfo1(request):
+	bikes= bike.objects.filter(operatorid=request.user)
+	return render(request, 'BikeOperators/rented_bikes.html', {'viewbike': bikes }) 
